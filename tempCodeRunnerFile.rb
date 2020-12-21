d52d92f05918c7f23065af1f1d7b50b5bb3cc28a
Perpaterb <zcarss@gmail.com>
2020-12-22 00:23:12 +1100

    board added

--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,2 +1,8 @@
-p board_by_row_forward
-# p board_by_column
+
+# board_by_row_forward = board.clone
+# board_by_row_backward = board.clone.reverse()
+# board_by_column = func_board_by_column(board)
+# board_by_up_left_diag = func_board_by_left_diag(board)
+# board_by_down_left_diag = board_by_up_left_diag.clone.reverse()
+# board_by_up_right_diag = func_board_by_right_diag(board)
+# board_by_down_right_diag = board_by_up_right_diag.clone.reverse()
