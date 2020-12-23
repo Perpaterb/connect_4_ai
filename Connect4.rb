a9a66ceef20b22a4f0af31e4e76e641af8aa0736
Perpaterb <zcarss@gmail.com>
2020-12-23 16:27:14 +1100

    game done.. AI next

--- Connect4.rb
+++ Connect4.rb
@@ -7,11 +7,17 @@ require "./minimax.rb"
 require "./move_made_update_board.rb"
 
 gamestate = "running"
-turn_of = "human"
+if rand(1..2) == 2
+    turn_of = "computer"
+else
+    turn_of = "human"
+end
 message = ""
 board = create_blank_board()
+
+
 while gamestate == "running"
-    #puts `clear`
+    puts `clear`
     print_board(board)
     win = test_for_a_win(board)
     if win[0] == true
@@ -21,31 +27,17 @@ while gamestate == "running"
         else
             puts "Computer wins!!"
         end
+        exit
     end
-    if turn_of == "computer"
+    case turn_of
+    when "computer"
         # AI time
-
-
-        print "#{message} Select 1 - 7 to make a move: "
-        p_move = gets.chomp.to_i
-        if (1..7).include? p_move
-            avalible = is_pos_avalible(p_move, board)
-            if avalible[0] == 1
-                board = move_made_update_board(p_move, turn_of, board, avalible[1])
-                turn_of = "human"
-            else
-                message = "Position not avalible!! Enter another position.. "
-            end
-        else
-            message = "invalid move!! please try again.. "
-        end
-
-
-
-
-
-    end  
-    if turn_of == "human"
+        
+        p_move = rand(1..7)
+        avalible = is_pos_avalible(p_move, board)
+        board = move_made_update_board(p_move, turn_of, board, avalible[1])
+        turn_of = "human"
+    when "human"
         print "#{message} Select 1 - 7 to make a move: "
         p_move = gets.chomp.to_i
         if (1..7).include? p_move
@@ -53,6 +45,7 @@ while gamestate == "running"
             if avalible[0] == 1
                 board = move_made_update_board(p_move, turn_of, board, avalible[1])
                 turn_of = "computer"
+                message = ""
             else
                 message = "Position not avalible!! Enter another position.. "
             end
--- move_made_update_board.rb
+++ move_made_update_board.rb
@@ -13,18 +13,15 @@ end
 
 def move_made_update_board(p_move, turn_of, board, row)
     i = board.find_index([row, p_move, "A"])
-    p i 
-    p board 
     if turn_of == "computer"
         board[i] = [row, p_move, "C"]
     else
         board[i] = [row, p_move, "H"]
     end
-    p board 
     new_a = board.find_index([row+1, p_move, "F"])
     if new_a == nil
     else
         board[new_a] = [row+1, p_move, "A"]
     end
-    p board 
+    return board
 end
